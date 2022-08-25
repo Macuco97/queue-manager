0e6215a403ac7efeccf2977ba751591095b941ab
unknown <rapha-sm2015@hotmail.com>
2022-08-25 07:30:55 -0300

    Table inserted

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import React from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useState, useEffect } from 'React'
-
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [client, setClient] = useState()
+
+ useEffect(() => {
+  const options = {method: 'GET', headers: {'Content-Type': 'application/json'}};
+
+  fetch('http://127.0.0.1:3001/api/database', options)
+    .then(response => response.json())
+    .then(response => setClient(response))
+    .catch(err => console.error(err));
+ },[])
+
   return (
     <>
       <div className = "flex flex-row h-14 bg-gradient-to-r from-gray-100 to-gray-200 ">
@@ -15,9 +26,28 @@ export default function Home() {
             Login
         </button>
       </div>
-      <div className="container mx-auto">
-        
-      </div>
+      <table class="border-collapse border border-slate-400 mx-auto w-9/12">
+          <thead>
+            <tr>
+              <th class="border border-slate-300 ...">State</th>
+              <th class="border border-slate-300 ...">City</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="border border-slate-300 ...">Indiana</td>
+              <td class="border border-slate-300 ...">Indianapolis</td>
+            </tr>
+            <tr>
+              <td class="border border-slate-300 ...">Ohio</td>
+              <td class="border border-slate-300 ...">Columbus</td>
+            </tr>
+            <tr>
+              <td class="border border-slate-300 ...">Michigan</td>
+              <td class="border border-slate-300 ...">Detroit</td>
+            </tr>
+          </tbody>
+      </table>
     </>
   )
 }
