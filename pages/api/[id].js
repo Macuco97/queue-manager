ac6ce55f82632816c3c187fea898d6bcf8a34de3
Raphael Motta <rapha-sm2015@hotmail.com>
2022-08-26 22:28:13 -0300

    26/08/2022 - 22:28

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,4 +1,6 @@
+import { UpdateOutlined } from "@mui/icons-material";
 import { ObjectID } from "bson";
+import { withTheme } from "styled-components";
 
 const { MongoClient } = require("mongodb");
 
@@ -8,33 +10,52 @@ export default async function (req, res){
   const client = new MongoClient(process.env.mongodbURL)
   const database = await client.db('client')
   const clients = await database.collection('client')
-  
   const { body, method } = req
+  const clientList = await clients.find({}).toArray()
+  const positions = []
+  const range = (start, end, length = end - start + 1) =>
+  Array.from({ length }, (_, i) => start + i)
+
+
+  let { position } = body
+
+  clientList.map(client => {
+    positions.push(client.position)
+  })
+  const highestPosition = positions.reduce(function(a, b) {
+    return Math.max(a, b);
+  }, -Infinity)
 
   if(req.method === "POST") {
-    const response = await clients.find({}).toArray()
-    if(response.length == [].length) {
-      console.log('Chegou aqui')
-      body.position = 1
+    if(clientList.length == [].length) {
+      body.position = 0
     }
     else {
       const positions = []
-      response.map(client => {
+      clientList.map(client => {
         positions.push(client.position)
       })
-      console.log(positions)
-      const highestPosition = positions.reduce(function(a, b) {
-        return Math.max(a, b);
-      }, -Infinity)
+      
       body.position = highestPosition + 1
       }
-    console.log(body)
     await clients.insertOne(body)
     }
   
 
   if(req.method === "DELETE") {
-    await clients.deleteOne({_id: ObjectID(body.id)})
+    const positionsChanged = range(position, positions.length - 1)
+    await clients.deleteOne({position: position})
+    if(positionsChanged.length == 1) {
+      await clients.deleteOne({position: position})
+    }
+    else {
+      positionsChanged.map(async i => {
+        if(i != positionsChanged - 1) {
+          await clients.updateOne({position: i + 1}, {$set:{position: i}})
+          await clients.find({position: i + 1})
+        }
+      })
+    }
   }
 
   const response = await clients.find({}).toArray()
--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,8 @@ const changeNewClientMenu = () => {
   setBlockScreen(!blockScreen)
 }
 
-const deleteClient = id => {
-  const options = {method: 'DELETE', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({id: id})}
+const deleteClient = position => {
+  const options = {method: 'DELETE', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({position: position})}
   fetch(`api/database`, options)
     .then(response => response.json())
     .then(response => setClient(response))
@@ -102,9 +102,6 @@ useEffect(() => {
                       return (
                         <tr className = {``}>
                           <td class="shadow-lg shadow-grey-500/50 border-b-4 border-r-4 border-slate-500 font-mono text-center text-3xl bg-lime-300">
-                            {item.position}
-                          </td>
-                          <td class="shadow-lg shadow-grey-500/50 border-b-4 border-r-4 border-slate-500 font-mono text-center text-3xl bg-lime-300">
                             {item.name}
                           </td>
                           <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
@@ -118,11 +115,12 @@ useEffect(() => {
                             </svg>
                           </td>
                           <td className = "shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
-                              onClick = {() => deleteClient(item._id)}    
+                              onClick = {() => deleteClient(item.position)}    
                           >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M5.383 12.25c.806 0 1.533-.446 2.031-1.08a9.04 9.04 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V4.75A.75.75 0 0113 4a2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H12.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H4.654m10.598-9.75H13M4.654 20.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368A12 12 0 011 17.125c0-1.553.295-3.036.831-4.398.306-.774 1.086-1.227 1.918-1.227h1.053c.472 0 .745.556.5.96A8.958 8.958 0 004 17.124c0 1.194.232 2.333.654 3.375z" />
                             </svg>
+
                           </td>
                         </tr>
                       )
@@ -168,7 +166,7 @@ useEffect(() => {
                             </svg>
                           </td>
                           <td className = "shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
-                              onClick = {() => deleteClient(item._id)}    
+                              onClick = {() => deleteClient(item.position)}    
                           >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
                               <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
