e2b13ff1165be74a4ef19898fd8858fcca6c8f18
unknown <rapha-sm2015@hotmail.com>
2022-08-28 09:20:11 -0300

    Login Feature Add

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -5,8 +5,18 @@ export default async function (req, res){
     const database = await client.db('client')
     const users = await database.collection('users')
     const { body, method } = req
-    const usersList = await users.find({}).toArray()
-   
-    res.status(200).json(usersList)
+    const collectionQuery = await users.find({}).toArray()
+    const usersList = collectionQuery[0]
+    let response
+    console.log(usersList[0])
+    console.log(usersList.user, usersList.password, body.user, body.password)
+    if (usersList.user == body.user && usersList.password == body.password) {
+        response = true
+    }
+    else {
+        response = false
+    }
+
+    res.status(200).json(response)
 }
 
--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,47 @@ const [client, setClient] = useState()
 const [newClient, setNewClient] = useState()
 const [login, setLogin] = useState()
 const [blockScreen, setBlockScreen] = useState()
+const [loginMenu, setLoginMenu] = useState()
 
+const loginAlert = loginStatus => {
+  if(loginStatus) {
+    setLogin(loginStatus)
+    changeLoginMenu()
+    alert("You Logged! Now you can create new Patient")
+  }
+  else {
+    alert("User or Password incorrect")
+  }
+}
 
 const changeNewClientMenu = () => {
   setNewClient(!newClient)
   setBlockScreen(!blockScreen)
+}
+
+const changeLoginMenu = () => {
+  setBlockScreen(!blockScreen)
+  setLoginMenu(!loginMenu)
+}
+
+const checkLogin = e => {
+  e.preventDefault()
+  const event = e.target
+  const user = event.user.value
+  const password = event.password.value
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      user: user, 
+      password: password
+    })
+  };
+  
+  fetch('http://127.0.0.1:3000/api/auth', options)
+    .then(response => response.json())
+    .then(response => loginAlert(response))
+    .catch(err => console.error(err));
 }
 
 const deleteClient = position => {
@@ -67,6 +103,24 @@ useEffect(() => {
         </form>
       </div>
     }
+    
+    {loginMenu && 
+      <div className = "flex w-screen h-screen fixed bg-transparent place-content-center place-items-center z-50">
+        <form onSubmit = {e => checkLogin(e) } className = {`relative w-1/3 h-1/6 flex flex-col flex-wrap gap-x-2 border-2 border-double bg-slate-400 p-2 place-content-center`}>
+            <label className = {``}>User </label>
+            <input id = 'user' className = {` pl-1 w-5/6`}/>
+            <label className = {``}>Password</label>
+            <input id = 'password' className = {` pl-1 w-5/6`}/>
+            <button className = 'justify-self-end bg-slate-300 mt-2 w-16 rounded-full '>
+              Login In
+            </button>
+            <svg onClick = {() => changeLoginMenu()} className="absolute top-1 right-1 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m-15 0l15 15" />
+            </svg>
+        </form>
+      </div>
+
+    }
     {
       blockScreen &&  
       <div className = "flex w-screen h-screen fixed bg-black opacity-75">
@@ -78,10 +132,15 @@ useEffect(() => {
           <div className = "text-2xl self-center align-middle basis-1/2 m-5">
             Queue Manager
           </div>
-          <button onClick = {() => changeNewClientMenu()} className = "bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full">
+          {login ? 
+            <button onClick = {() => changeNewClientMenu()} className = {`bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full`} >
               Adicionar Cliente
-          </button>
-          <button className = "bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full">
+            </button>
+            :
+            <button  className = {`basis-1/4 h-9/12 m-3 invisible`} >
+            </button>
+          }
+          <button onClick = {() => changeLoginMenu()} className = "bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full">
               Login
           </button>
         </div>
@@ -155,24 +214,28 @@ useEffect(() => {
                           <td class="shadow-lg shadow-grey-500/50 border-b-4 border-r-4 border-slate-500 font-mono text-center text-3xl bg-gray-100">
                             {item.name}
                           </td>
-                          <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
-                            </svg>
-                          </td>
-                          <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
-                            </svg>
-                          </td>
-                          <td className = "shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
-                              onClick = {() => deleteClient(item.position)}    
-                          >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
-                              <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg>
-                          </td>
-
+                          {
+                          login && 
+                          <>
+                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+                              </svg>
+                            </td>
+                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
+                              </svg>
+                            </td>
+                            <td className = "shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
+                                onClick = {() => deleteClient(item.position)}    
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                              </svg>
+                            </td>
+                          </>
+                          }
                         </tr>
                       )
                     
