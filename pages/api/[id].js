236bb920af668dff8f4d6bc2e11822e25ca55207
Raphael Motta <rapha-sm2015@hotmail.com>
2022-08-31 10:45:02 -0300

    Loading feature complete

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,4 +1,4 @@
-import { UpdateOutlined } from "@mui/icons-material";
+import { ConstructionOutlined, UpdateOutlined } from "@mui/icons-material";
 import { ObjectID } from "bson";
 import { withTheme } from "styled-components";
 
@@ -15,9 +15,7 @@ export default async function (req, res){
   const positions = []
   const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i)
-
-
-  let { position } = body
+  let { position, name, phone, action } = body
 
   clientList.map(client => {
     positions.push(client.position)
@@ -27,27 +25,43 @@ export default async function (req, res){
   }, -Infinity)
 
   if(req.method === "POST") {
-    if(clientList.length == [].length) {
-      body.position = 0
-    }
-    else {
-      const positions = []
-      clientList.map(client => {
-        positions.push(client.position)
-      })
-      
-      body.position = highestPosition + 1
+      if(clientList.length == [].length) {
+        body.position = 0
       }
-    await clients.insertOne(body)
+      else {
+        const positions = []
+        clientList.map(client => {
+          positions.push(client.position)
+        })
+        
+        body.position = highestPosition + 1
+        }
+      await clients.insertOne(body)
     }
   
+ if(req.method === "PUT") {
+    if (body.action === "goDown") {
+      const currentClient = await clients.findOne({position: position})
+      const beforeClient = await clients.findOne({position: position+1})
+      await clients.updateOne({_id: currentClient._id}, {$inc: {position: 1}})
+      await clients.updateOne({_id: beforeClient._id}, {$inc:{position: -1}})
+    }
+    else if (body.action === "goUp") {
+      const currentClient = await clients.findOne({position: position})
+      const nextClient = await clients.findOne({position: position-1})
+      await clients.updateOne({_id: currentClient._id}, {$inc: {position: -1}})
+      await clients.updateOne({_id: nextClient._id}, {$inc:{position: 1}})
+      
+    }
+  }
+
   if(req.method === "DELETE") {
     await clients.deleteOne({position: position})
 
     await clients.updateMany({position: {$gt: position}}, {$inc:{position: -1}})
   }
 
-  const response = await clients.find({}).toArray()
+  const response = await clients.find({}).sort({position: 1}).toArray()
   await client.close()
   res.status(200).json(response)
 }
--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,25 @@ const [newClient, setNewClient] = useState()
 const [login, setLogin] = useState()
 const [blockScreen, setBlockScreen] = useState()
 const [loginMenu, setLoginMenu] = useState()
+const [loading, setLoading] = useState()
+
+const changeOrder = (position, action) => {
+  setLoading(true)
+  const options = {
+    method: 'PUT',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      position: position, 
+      action: action
+    })
+  }
+
+  fetch('http://127.0.0.1:3000/api/database', options)
+    .then(response => response.json())
+    .then(response => setClient(response))
+    .then(response => setLoading(false))
+    .catch(err => console.error(err));
+}
 
 const loginAlert = loginStatus => {
   if(loginStatus) {
@@ -33,6 +52,7 @@ const changeLoginMenu = () => {
 }
 
 const checkLogin = e => {
+  setLoading(true)
   e.preventDefault()
   const event = e.target
   const user = event.user.value
@@ -49,18 +69,22 @@ const checkLogin = e => {
   fetch('http://127.0.0.1:3000/api/auth', options)
     .then(response => response.json())
     .then(response => loginAlert(response))
+    .then(response => setLoading(false))
     .catch(err => console.error(err));
 }
 
 const deleteClient = position => {
+  setLoading(true)
   const options = {method: 'DELETE', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({position: position})}
   fetch(`api/database`, options)
     .then(response => response.json())
     .then(response => setClient(response))
+    .then(response => setLoading(false))
     .catch(err => console.error(err));
 }
 
 const createClient = e => {
+  setLoading(true)
   e.preventDefault()
   const event = e.target
   const name = event.PatientNameInput.value
@@ -73,14 +97,17 @@ const createClient = e => {
   fetch(`api/database`, options)
     .then(response => response.json(response))
     .then(response => setClient(response))
+    .then(response => setLoading(false))
     .catch(err => console.error(err))
   }
 
 useEffect(() => {
+  setLoading(true)
   const options = {method: 'GET', headers: {'Content-Type': 'application/json'}};
   fetch('api/database', options)
     .then(response => response.json())
     .then(response => setClient(response))
+    .then(response => setLoading(false))
     .catch(err => console.error(err));
  },[])
 
@@ -94,7 +121,7 @@ useEffect(() => {
             <input id = 'PatientNameInput' className = {` pl-1 w-5/6`}/>
             <label id = 'PatientPhoneLabel' className = {``}>Telefone </label>
             <input id = 'PatientPhoneInput' className = {` pl-1 w-5/6`}/>
-            <button className = 'justify-self-end bg-slate-300 mt-2 w-16 rounded-full '>
+            <button disabled = {loading} className = {`justify-self-end bg-slate-300 mt-2 w-16 rounded-full ${loading && "opacity-50"}`}>
               Criar
             </button>
             <svg onClick = {() => changeNewClientMenu()} className="absolute top-1 right-1 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
@@ -111,7 +138,7 @@ useEffect(() => {
             <input id = 'user' className = {` pl-1 w-5/6`}/>
             <label className = {``}>Password</label>
             <input id = 'password' className = {` pl-1 w-5/6`}/>
-            <button className = 'justify-self-end bg-slate-300 mt-2 w-16 rounded-full '>
+            <button disabled = {loading} className = {`disabled justify-self-end bg-slate-300 mt-2 w-16 rounded-full ${loading && "opacity-50"}`}>
               Login In
             </button>
             <svg onClick = {() => changeLoginMenu()} className="absolute top-1 right-1 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
@@ -123,21 +150,27 @@ useEffect(() => {
     }
     {
       blockScreen &&  
-      <div className = "flex w-screen h-screen fixed bg-black opacity-75">
+      <div className = "flex w-screen h-screen fixed bg-black opacity-50 z-40">
       </div>
     }
-      <div className = {`flex flex-col w-screen h-screen mx-0`}>
-
+      <div className = {`flex flex-col w-screen h-screen mx-0 relative`}>
         <div className = "flex flex-row h-20 bg-gradient-to-r from-gray-100 to-gray-200 ">
-          <div className = "text-2xl self-center align-middle basis-1/2 m-5">
-            Queue Manager
+          {
+          loading && 
+          <svg class="right-0 absolute mr-2 mt-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          }
+          <div className = "text-2xl self-center align-middle basis-4/5 m-5">
+            Queue Manager {loading}
           </div>
           {login ? 
-            <button onClick = {() => changeNewClientMenu()} className = {`bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full`} >
+            <button onClick = {() => changeNewClientMenu()} className = {`bg-gray-300 hover:bg-gray-400 basis-4/5 h-9/12 m-3 rounded-full`} >
                Adicionar Cliente
             </button>
             :
-            <button onClick = {() => changeLoginMenu()} className = "bg-gray-300 hover:bg-gray-400 basis-1/4 h-9/12 m-3 rounded-full">
+            <button disabled = {loading} onClick = {() => changeLoginMenu()} className = {`bg-gray-300 hover:bg-gray-400 h-9/12 m-3 rounded-full justify-self-end grow ${loading && `opacity-50`}`}>
               Login
             </button>
           }
@@ -207,16 +240,23 @@ useEffect(() => {
                           {
                           login && 
                           <>
-                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
+                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
+                                onClick = {() => changeOrder(item.position, 'goUp')}
+                            >
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
                               </svg>
                             </td>
-                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10">
+                           { 
+                           (index + 1 != client.length) && 
+                            <td className="shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
+                                onClick = {() => changeOrder(item.position, 'goDown')}
+                            >
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />
                               </svg>
                             </td>
+                            }
                             <td className = "shadow-lg shadow-grey-500/50 border-b-4 border-slate-500 font-mono text-center bg-gray-100 w-10 h-10"
                                 onClick = {() => deleteClient(item.position)}    
                             >
